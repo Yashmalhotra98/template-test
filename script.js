183628ec7485ff986dba3c1964b5f168cae6e47f
Yashmalhotra98 <Blackhacker1998@Gmail.com>
2021-12-19 13:50:06 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Our browser window is represented by window in JS by default all JS is in our window element. 
+// Within the window object, we have a console object which allows us to `log` our data in the in the browser window. 
+//  This is helpful in many ways but the most important is that it allows us to see the data in the browser window.
+//  FOr ex if  we call an API and we get back some data, we can log that data to the browser window.
+window.console.log()
